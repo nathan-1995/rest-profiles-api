faa2af3adc5644e14db12a86e14ce21463a07334
Nathan De Lima <ndelima1@gmail.com>
2023-05-19 00:27:57 +0530

    added post, patch,put and delete

---
+++ profiles_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class HelloSerializers(serializers.Serializer):
+
+    """Serializer a name field for testing our apiview"""
+    name = serializers.CharField(max_length=10)
+    age = serializers.IntegerField()
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializers
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -16,3 +20,27 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create message with our name"""
+        serializers = self.serializer_class(data=request.data)
+
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            age = serializers.validated_data.get('age')
+            message = f'Hello {name}! You are {age} years old.'
+            return Response({'message': message})
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
